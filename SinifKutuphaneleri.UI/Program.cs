efaed834df6c09014bbe94ba66812286fbf8e794
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2021-12-04 13:48:32 +0300

    generic classlar ve generic yapılar

---
+++ SinifKutuphaneleri.UI/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SinifKutuphaneleri.UI
+{
+    class Program
+    {
+
+        // Main metodu bir console uygulamasını yürüyen metottur.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
