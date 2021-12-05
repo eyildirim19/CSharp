e35bb1f26ad73486bec038bffc510261888426e4
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2021-12-05 12:33:09 +0300

    linq sorguları örnek

--- Linq2/Program.cs
+++ Linq2/Program.cs
@@ -38,6 +38,50 @@ namespace Linq2
                 ogrencis[i].Notu = n; // sıradaki öğrencinin notuna n nesnesini set et...
             }
 
+
+
+            // not: select ifadesiden seçilen obje tipinde değer dönüşür
+
+            // linq sorgularına başlayalım....
+            // Cinsiyeti erken olan öğrenciler...
+            List<Ogrenci> baylar = (from o in ogrencis
+                                    where o.Cinsiyet == 'e'
+                                    select o).ToList(); // öğrenciyi seç
+
+            // cinsiyeti k olan ve ortalaması 50 den byük olan
+            List<Ogrenci> bayanlar = (from b in ogrencis
+                                      where b.Cinsiyet == 'k' && b.Notu.Ortalama > 50
+                                      select b).ToList();
+
+            //notu 50'den büyük olanların adsoyadlrı
+            List<string> isimler = (from j in ogrencis
+                                    where j.Notu.Ortalama > 50
+                                    select j.AdiSoyAdi).ToList(); // geriye string dön...
+
+            //notu 50'den küçük olanların cinsiyet
+            List<char> cinsyeyler = (from c in ogrencis
+                                     where c.Notu.Ortalama < 50
+                                     select c.Cinsiyet).ToList();
+
+            // notu 50'den büyük olanları adsoyoad ve cinsiyetleri
+            // Not : select ifadesinde new komutu kullanılırsa sorgudan dönen sonuç anonimdir...
+            var result = (from c in ogrencis
+                          where c.Notu.Ortalama > 50
+                          select new
+                          {
+                              c.AdiSoyAdi,
+                              c.Cinsiyet
+                          }).ToList();
+
+            foreach (var item in result)
+                Console.WriteLine($"{item.AdiSoyAdi} {item.Cinsiyet}");
+
+            // sınıf ortlamasını bulalım...
+            float ortalam = ogrencis.Sum(c => c.Notu.Ortalama) / ogrencis.Count;
+            float ortalama = ogrencis.Average(c => c.Notu.Ortalama);
+
+
+
             Console.ReadKey();
         }
     }
